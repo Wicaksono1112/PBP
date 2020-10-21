0808262877ad237d8600adb109d8dd901552e6af
Wicaksono1112 <51833860+Wicaksono1112@users.noreply.github.com>
2020-10-21 22:54:38 +0700

    Update error_php.php

--- application/views/errors/html/error_php.php
+++ application/views/errors/html/error_php.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <h4>A PHP Error was encountered</h4>
 
-<p>Severity: <?php echo $severity; ?></p>
+<p>Severity: <?php return $severity; ?></p>
 <p>Message:  <?php echo $message; ?></p>
 <p>Filename: <?php echo $filepath; ?></p>
 <p>Line Number: <?php echo $line; ?></p>
